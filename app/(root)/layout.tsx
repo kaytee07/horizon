e1191a215b1f39710db68bf12eeab25358ecb33c
kaytee07 <kofi.taylor07@gmail.com>
2024-05-06 17:41:32 +0000

    layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        SIDEBAR
+        {children}
+    </main>
+  );
+}
